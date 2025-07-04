9ef38971dd34931ff3b986a24d0d7064b087a928
nodai2hITC <nodai2h.itc@gmail.com>
2025-07-04 17:56:45 +0900

    入力コード自動生成に対応できるケースを追加

--- python_make_input.js
+++ python_make_input.js
@@ -9,21 +9,27 @@ BatchCodeTest.make_input = function(_input, constraints) {
         return "int"
     }
     const varname = function(name) {
-        console.log(name)
-        if (_input.match(new RegExp(`(?:\\s|^)${RegExp.escape(name).toUpperCase()}[\\s_]`)) &&
-            _input.match(new RegExp(`(?:\\s|^)${RegExp.escape(name).toLowerCase()}[\\s_]`))) return name
+        if (_input.match(new RegExp(`(?:\\s|^)${RegExp.escape(name.toUpperCase())}[\\s_]`)) &&
+            _input.match(new RegExp(`(?:\\s|^)${RegExp.escape(name.toLowerCase())}[\\s_]`))) return name
         return name.toLowerCase()
     }
 
     let code = ""
     while (input != "") {
         let matched
-        if (matched = input.match(/^\s*((.).*)\n(?:\2.*\n)*?.*\\vdots.*\n\2(?:[^_]*_\{?|\s*query\s*)([^},) ]+).*\n/i)) {
+        if (matched = input.match(/^\s*((.).*)\n(?:\2.*\n)*?.*\\vdots.*\n\2(?:[^_]*_\{?|\s*query\s*(?:[^a-z]+\s*)?)([^},) ]+).*\n/i)) {
             // 複数行の場合
             const n = matched[3]
             const line = matched[1]
             let matched2
-            if (matched2 = line.match(/^([a-z]).*[cl]?dots/i)) {
+            if (matched2 = line.match(/^([a-z])_1 ([a-z])_\{1,\s*1\}.*[cl]?dots/i)) {
+                // 先頭が長さ、次以降が配列の場合
+                const vn = varname(n), v1 = varname(matched2[1]), v2 = varname(matched2[2])
+                code += `${v1} = [0] * ${vn.match(/[\+\-\*\/]/) ? `(${vn})` : vn}\n`
+                code += `${v2} = [0] * ${vn.match(/[\+\-\*\/]/) ? `(${vn})` : vn}\n`
+                code += `for i in range(${vn}):\n`
+                code += `    ${v1}[i], *${v2}[i] = list(map(int, input().split()))\n`
+            } else if (matched2 = line.match(/^([a-z]).*[cl]?dots/i)) {
                 // 2次元配列の場合
                 const v = matched2[1]
                 const type = get_type(v)
@@ -32,12 +38,12 @@ BatchCodeTest.make_input = function(_input, constraints) {
                 if (type == "float") code += `[list(map(float, input().split())) for _ in range(${varname(n)})]\n`
                 if (type == "str"  ) code += `[input().split() for _ in range(${varname(n)})]\n`
                 if (type == "char" ) code += `[input() for _ in range(${varname(n)})]\n`
-            } else if (matched2 = line.match(/query/i)) {
+            } else if (matched2 = line.match(/(query|event)/i)) {
                 // クエリの場合
                 if (constraints.match(/入力.*は(?:すべて|全て)整数/)) {
-                    code += `query = [list(map(int, input().split())) for _ in range(${varname(n)})]\n`
+                    code += `${varname(matched2[1])} = [list(map(int, input().split())) for _ in range(${varname(n)})]\n`
                 } else {
-                    code += `query = [input().split() for _ in range(${varname(n)})]\n`
+                    code += `${varname(matched2[1])} = [input().split() for _ in range(${varname(n)})]\n`
                 }
             } else if (matched2 = line.match(/\d\}?\s*[^}\s]/i)) {
                 // 複数変数の場合
@@ -59,7 +65,7 @@ BatchCodeTest.make_input = function(_input, constraints) {
                 code += `${varname(v)} = `
                 if (type == "int"  ) code += `[int(input()) for _ in range(${varname(n)})]\n`
                 if (type == "float") code += `[float(input()) for _ in range(${varname(n)})]\n`
-                if (type == "str"  ) code += `[input() for _ in range(${varname(n)})]\n`
+                if (type == "str" || type == "char") code += `[input() for _ in range(${varname(n)})]\n`
             } else {
                 code += "# 最後まで入力コードの生成ができませんでした。ご注意ください。\n"
                 break
--- scrape/atcoder.js
+++ scrape/atcoder.js
@@ -7,7 +7,7 @@ BatchCodeTest.scrapes.push({
         const inputs  = Array.from(html.matchAll(/<h3>入力例 *\d+<\/h3>\s*<pre>\s*(.+?)\s*<\/pre>/gis), m => m[1]);
         const outputs = Array.from(html.matchAll(/<h3>出力例 *\d+<\/h3>\s*<pre>\s*(.+?)\s*<\/pre>/gis), m => m[1]);
         let input = undefined;
-        const matched1 = html.match(/<h3>入力<\/h3>\s*<p>入力は以下の形式で標準入力から与えられる[^<]*<\/p>\s*<pre>(.+?)<\/pre>/s)
+        const matched1 = html.match(/<h3>入力<\/h3>\s*<p>入力は以下の形式で(?:[^<]|<\/?var>)*<\/p>\s*<pre>(.+?)<\/pre>/s)
         if (matched1) {
             input = matched1[1].replaceAll(/<[^>]*>/g, " ").replaceAll(/  +/g, " ").replaceAll(/ \n/g, "\n").replaceAll(/\n /g, "\n");
         }
